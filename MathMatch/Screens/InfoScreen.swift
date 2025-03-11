cddf9d4940b663f12f61293fdf0b217f61b898a5
Güneş Akgün <gunesakgun9@gmail.com>
2025-03-11 14:04:11 +0300

    change icons

---
+++ MathMatch/Screens/InfoScreen.swift
@@ -0,0 +1,18 @@
+//
+//  InfoScreen.swift
+//  MathMatcher
+//
+//  Created by Gunes Akgun on 2.10.2024.
+//
+
+import SwiftUI
+
+struct InfoScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoScreen()
+}
